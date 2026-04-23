6a4e8613b5423837738217c9278cb1e841d97f7e
nekit54567 <kurashkin21@yandex.ru>
2026-04-24 00:38:53 +0300

    fix router order

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,12 @@
 from aiogram import Dispatcher
 from .start     import router as start_router
-from .solver    import router as solver_router
 from .schedule  import router as schedule_router
 from .deadlines import router as deadline_router
+from .solver    import router as solver_router
 
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(start_router)
-    dp.include_router(solver_router)
     dp.include_router(schedule_router)
     dp.include_router(deadline_router)
+    dp.include_router(solver_router)
